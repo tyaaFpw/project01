23331f82cca2247ea9578650283b70a512838d1d
Tyaa <gratia@icehousecorp.com>
2016-03-16 16:57:07 +0700

    Initial Commit

---
+++ project01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project01
+//
+//  Created by Tyaa on 3/16/16.
+//  Copyright © 2016 Tyaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project01
+//
+//  Created by Tyaa on 3/16/16.
+//  Copyright © 2016 Tyaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
