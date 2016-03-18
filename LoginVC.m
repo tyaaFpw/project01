49a3c230b401b83283cd2dc148ff017ae9de1b4b
Tyaa <gratia@icehousecorp.com>
2016-03-18 13:50:45 +0700

    Updating ViewCtrler for Facebook and Twitter Login

---
+++ LoginVC.m
@@ -0,0 +1,47 @@
+//
+//  loginVC.m
+//  project01
+//
+//  Created by Tyaa on 3/17/16.
+//  Copyright © 2016 Tyaa. All rights reserved.
+//
+
+#import "LoginVC.h"
+#import "FBLoginObject.h"
+
+@interface LoginVC ()
+
+@property (weak, nonatomic) IBOutlet UIButton *btngplus;
+@property (weak, nonatomic) IBOutlet UIButton *btntwitter;
+@property (weak, nonatomic) IBOutlet UIButton *btnfacebook;
+
+@property (strong, nonatomic) FBLoginObject *loginObject;
+
+@end
+
+@implementation LoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)didTappedBtnGPlus:(id)sender {
+}
+
+- (IBAction)didTappedBtnTwitter:(id)sender {
+}
+
+- (IBAction)didTappedBtnFacebook:(id)sender {
+    
+    self.loginObject = [[FBLoginObject alloc]init];
+    [self.loginObject fbLoginManager];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
